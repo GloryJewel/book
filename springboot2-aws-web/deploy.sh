30b5747b01d841cb3ff1db3aa41012287f5a264e
sgloryjewel@gmail.com <wnslagksk0>
2020-07-01 13:54:34 +0900

    shell

--- springboot2-aws-web/deploy.sh
+++ springboot2-aws-web/deploy.sh
@@ -13,7 +13,7 @@ git pull
 
 echo "> build start"
 
-./gradlew build
+../gradlew build
 
 echo "> move $REPOSITORY"
 
@@ -43,6 +43,6 @@ JAR_NAME=$(ls -tr $REPOSITORY/ | grep *.jar | tail -n 1)
 
 echo "> JAR name: $JAR_NAME"
 
-nohup java -jar -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-oauth.yml $REPOSITORY/$JAR_NAME 2>&1 &
+nohup java -jar -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real.yml $REPOSITORY/$JAR_NAME 2>&1 &
 
 
