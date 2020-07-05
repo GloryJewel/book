461273872821fb31ccc943523c948b3ea7c13a42
sgloryjewel@gmail.com <wnslagksk0>
2020-07-05 22:35:25 +0900

    bin/bash

--- springboot2-aws-web/scripts/step2/deploy.sh
+++ springboot2-aws-web/scripts/step2/deploy.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #REPOSITORY git clone, REPOSITORY에 jar 파일 이동 및 실행
 
 REPOSITORY=/home/ec2-user/app/step2
@@ -26,11 +27,13 @@ echo "> deploy application"
 
 JAR_NAME=$(ls -tr ${REPOSITORY}/ | grep *.jar | tail -n 1)
 
-echo "> JAR name: "${JAR_NAME}""
+echo "> JAR name: ${JAR_NAME}"
 
 echo "> ${REPOSITORY}/${JAR_NAME} add execute permission"
 
 chmod +x ${REPOSITORY}/${JAR_NAME}
+
+cd ${REPOSITORY}
 
 nohup java -jar -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real.yml ${REPOSITORY}/${JAR_NAME} 2>&1 &
 
