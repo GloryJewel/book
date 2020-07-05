f63307ceebb418c863bd7c7c91bea4d822bd2424
sgloryjewel@gmail.com <wnslagksk0>
2020-07-05 23:57:42 +0900

    nohup.out

--- springboot2-aws-web/scripts/step2/deploy.sh
+++ springboot2-aws-web/scripts/step2/deploy.sh
@@ -32,6 +32,4 @@ echo "> ${JAR_NAME} add execute permission"
 
 chmod +x ${JAR_NAME}
 
-cd ${REPOSITORY}
-
-nohup java -jar -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real.yml ${JAR_NAME} 2>&1 &
+nohup java -jar -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real.yml ${JAR_NAME} > ${REPOSITORY}/nohup.out 2>&1 &
