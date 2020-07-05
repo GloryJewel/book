30a3a80d85d0c46e22c1af764f599ee45274a6a5
sgloryjewel@gmail.com <wnslagksk0>
2020-07-05 22:25:26 +0900

    ...

--- springboot2-aws-web/scripts/step2/deploy.sh
+++ springboot2-aws-web/scripts/step2/deploy.sh
@@ -26,12 +26,12 @@ echo "> deploy application"
 
 JAR_NAME=$(ls -tr ${REPOSITORY}/ | grep *.jar | tail -n 1)
 
-echo "> JAR name: $JAR_NAME"
+echo "> JAR name: "${JAR_NAME}""
 
 echo "> ${REPOSITORY}/${JAR_NAME} add execute permission"
 
 chmod +x ${REPOSITORY}/${JAR_NAME}
 
-nohup java -jar -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real.yml $REPOSITORY/$JAR_NAME 2>&1 &
+nohup java -jar -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real.yml ${REPOSITORY}/${JAR_NAME} 2>&1 &
 
 
