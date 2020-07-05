27e76c800e78dcc44765e6b2684a691c83f3603a
sgloryjewel@gmail.com <wnslagksk0>
2020-07-05 18:34:13 +0900

    deploy

---
+++ springboot2-aws-web/scripts/step2/deploy.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+#REPOSITORY git clone, REPOSITORY에 jar 파일 이동 및 실행
+
+REPOSITORY=/home/ec2-user/app/step2
+PROJECT_ROOT=book
+PROJECT_NAME=springboot2-aws-web
+
+echo "> build 파일복사"
+
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> confirm the running program pid"
+
+CURRENT_PID=$(pgrep -f -${PROJECT_NAME}*.jar)
+
+echo "> running program pid: $CURRENT_PID"
+
+if [-z "$CURRENT_PID"]; then
+    echo "> there are no programs running"
+else
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
+fi
+
+echo "> deploy application"
+
+JAR_NAME=$(ls -tr $REPOSITORY/ | grep *.jar | tail -n 1)
+
+echo "> JAR name: $JAR_NAME"
+
+echo "> $JAR_NAME add execute permission"
+
+chmod +x $JAR_NAME
+
+nohup java -jar -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real.yml $REPOSITORY/$JAR_NAME 2>&1 &
+
+
