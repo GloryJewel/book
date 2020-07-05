cb29b989da1ae7f48b06c7a6c1a8e319b135a46e
sgloryjewel@gmail.com <wnslagksk0>
2020-07-05 22:45:52 +0900

    bin/bash

--- springboot2-aws-web/scripts/step2/deploy.sh
+++ springboot2-aws-web/scripts/step2/deploy.sh
@@ -2,7 +2,6 @@
 #REPOSITORY git clone, REPOSITORY에 jar 파일 이동 및 실행
 
 REPOSITORY=/home/ec2-user/app/step2
-PROJECT_ROOT=book
 PROJECT_NAME=springboot2-aws-web
 
 echo "> build 파일복사"
@@ -13,12 +12,12 @@ echo "> confirm the running program pid"
 
 CURRENT_PID=$(pgrep -f -${PROJECT_NAME}*.jar)
 
-echo "> running program pid: $CURRENT_PID"
+echo "> running program pid: ${CURRENT_PID}"
 
-if [-z "$CURRENT_PID"]; then
+if [-z "${CURRENT_PID}"]; then
     echo "> there are no programs running"
 else
-    echo "> kill -15 $CURRENT_PID"
+    echo "> kill -15 ${CURRENT_PID}"
     kill -15 ${CURRENT_PID}
     sleep 5
 fi
